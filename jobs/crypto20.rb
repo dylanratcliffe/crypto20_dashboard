05e5291b15bbfb99cc375e3eb136451e4b3fa122
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-10-22 22:35:33 +0800

    Changed chart update period to 8min

--- jobs/crypto20.rb
+++ jobs/crypto20.rb
@@ -1,9 +1,11 @@
 require 'rest-client'
 require 'json'
 
-points = []
-labels = []
-last_x = 1
+requests      = []
+points        = []
+labels        = []
+last_x        = 1
+current_value = 0
 
 SCHEDULER.every '3s' do
   # Do all external requests in parallel
@@ -25,21 +27,13 @@ SCHEDULER.every '3s' do
   # Wait for all to complete
   threads.each { |t| t.join }
 
+  # store the main response to save code
   response = requests[:main]
 
-  last_x += 1
-  points << { x: last_x, y: response['usd_value'] }
-  labels << Time.new.to_i
-
-  # Limit the amount of data
-  data_limit = 290
-  if points.length > data_limit
-    points = points.drop(points.length - data_limit)
-  end
-  if labels.length > data_limit
-    labels = labels.drop(labels.length - data_limit)
-  end
-  send_event('usd_value', { points: points, labels: labels })
+  # Get current value for graph
+  current_value = requests[:main]['usd_value']
+  # Send an event to update the number but not the whole graph
+  send_event('usd_value', value: current_value)
 
   # Calculate percetage
   growth = (((response['usd_value'].to_f / response['presale'].to_f) - 1)*100).round(1)
@@ -86,4 +80,21 @@ SCHEDULER.every '3s' do
   send_event('ltc_received', current: response['ltc_received'])
   send_event('eth_received', current: response['eth_received'])
   send_event('backers', current: response['backers'])
+end
+
+
+SCHEDULER.every '8m' do
+  last_x += 1
+  points << { x: last_x, y: current_value }
+  labels << Time.new.to_i
+
+  # Limit the amount of data
+  data_limit = 290
+  if points.length > data_limit
+    points = points.drop(points.length - data_limit)
+  end
+  if labels.length > data_limit
+    labels = labels.drop(labels.length - data_limit)
+  end
+  send_event('usd_value', { points: points, labels: labels })
 end
--- widgets/line/line.coffee
+++ widgets/line/line.coffee
@@ -1,16 +1,22 @@
 class Dashing.Line extends Dashing.Chartjs
 
   @accessor 'current', ->
-    points = @get('points')
-    if points
-      points[points.length - 1].y
+    value = @get('value')
+    if value
+      value
+
 
   ready: ->
-    @chart = @lineChart("line",@get('points'),@get('labels'))
+    @chart   = @lineChart("line",@get('points'),@get('labels'))
+    @current = @get('value')
     @chart
 
   onData: (data) ->
     if @chart
-      @chart.config.data.datasets[0].data = data.points
-      @chart.config.data.labels = data.labels
-      @chart.update()
+      if data.value
+        @current = data.value
+        @chart.update()
+      if data.points
+        @chart.config.data.datasets[0].data = data.points
+        @chart.config.data.labels = data.labels
+        @chart.update()

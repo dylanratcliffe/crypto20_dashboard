79f5ec0c738f3a7521c81725b1caeea79fe97984
TheMadCanudist <themadcanudist@theendless.org>
2017-11-21 20:15:15 -0500

    Use new crypto20 API and add BTG to NAV

--- jobs/crypto20.rb
+++ jobs/crypto20.rb
@@ -8,38 +8,6 @@ last_x        = 1
 current_value = 0
 current_nav   = 0.to_f
 
-# Temporarily added until we get an API
-# This is the amount held in cold, we will add live values to this
-COLD_FUNDS = {
-  bitcoin:              190.12,
-  ethereum:             4262.22,
-  'bitcoin-cash':       1252.00,
-  ripple:               6349758.35,
-  litecoin:             19430.30,
-  dash:                 2701.944949,
-  neo:                  22147.93,
-  iota:                 943340.00,
-  monero:               5338.31,
-  nem:                  3104649.88,
-  'ethereum-classic':   34130.78,
-  lisk:                 39875.55,
-  qtum:                 25012.39,
-  eos:                  168518.57,
-  hshare:               15338.38,
-  omisego:              36174.67,
-  zcash:                945.62,
-  cardano:              9163713.54,
-  stellar:              6282537.55,
-  waves:                34920.00,
-  'bitcoin-gold':       458.00,
-}
-
-SPENT_FUNDS = {
-  ETH: 24370,
-  BTC: 707,
-  LTC: 14461,
-}
-
 COLORS = {
   BTC:   "#F7931A",
   BCH:   "#F7931A",
@@ -80,18 +48,6 @@ COLORS = {
   BTG:   "#F7931A",
 }
 
-def current_fund_value(crypto_prices,funds_held)
-  total_value = 0.0
-
-  funds_held.each do |coin,count|
-    price = crypto_prices.select{|c| c['id'] == coin.to_s}[0]['price_usd'].to_f
-    value = price * count.to_f
-    total_value += value
-  end
-
-  total_value.round(0)
-end
-
 # If we have data we ripped from the old /events endpoint, read it in
 SCHEDULER.in '0s' do
   file = File.read('bootstrap_data/points.json')
@@ -109,7 +65,7 @@ SCHEDULER.every '3s' do
   # Do all external requests in parallel
   requests = {
     main: 'https://www.crypto20.com/status',
-    all:  'https://api.coinmarketcap.com/v1/ticker/?limit=2000',
+    btg:  'https://api.coinmarketcap.com/v1/ticker/bitcoin-gold/',
   }
 
   threads = []
@@ -121,26 +77,23 @@ SCHEDULER.every '3s' do
   # store the main response to save code
   response = requests[:main]
 
-  # Get the current funds held by combining cold and hot funds
-  funds = COLD_FUNDS.clone
-  funds[:ethereum] += requests[:main]['eth_received'] - SPENT_FUNDS[:ETH]
-  funds[:bitcoin] += requests[:main]['btc_received'] - SPENT_FUNDS[:BTC]
-  funds[:litecoin] += requests[:main]['ltc_received'] - SPENT_FUNDS[:LTC]
-
+  # Get bitcoin-gold value (we have approx 458 coins)
+  btg_usd = requests[:btg][0]['price_usd'].to_i * 458
+  response['holdings'] << { 'name' => 'BTG', 'value' => btg_usd }
+  
   # Get current value for graph
-  current_value = current_fund_value(requests[:all],funds)
+  current_value = response['usd_value'].to_f + btg_usd.to_f
 
   # Send an event to update the number but not the whole graph
   send_event('usd_value', value: current_value)
 
   # Calculate split
   split = []
-  funds.each do |id,count|
-    coin_stats = requests[:all].select{|c| c['id'] == id.to_s}[0]
+  response['holdings'].each do |asset|
     split << {
-      value: coin_stats['price_usd'].to_f * count.to_f,
-      color: COLORS[coin_stats['symbol'].to_sym],
-      label: coin_stats['name']
+      value: asset['value'],
+      color: COLORS[asset['name'].to_sym],
+      label: asset['name']
     }
   end
 
@@ -155,7 +108,12 @@ SCHEDULER.every '3s' do
   send_event('split', data: data)
 
   # Calculate NAV
-  current_nav = ((current_value.to_f / response['presale'].to_f)* 0.87) * 0.98
+  current_nav = response['nav_per_token']
+
+  # BTG component of NAV
+  btg_nav = btg_usd.to_f / response['presale'].to_f * 0.98 * 0.87
+  current_nav += btg_nav
+
   percent     = ((current_nav - 1) / 1.00) * 100
   # If the percentage is negative, make it positive and set the arrow to down
   if percent < 0
